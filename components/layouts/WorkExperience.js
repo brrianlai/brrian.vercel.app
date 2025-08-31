7f1c692d94e650a489289c6b10343f22692a0522
brrianlai <iibriaan@gmail.com>
2025-08-30 17:22:31 -0700

    Update WorkExperience.js

--- components/layouts/WorkExperience.js
+++ components/layouts/WorkExperience.js
@@ -13,12 +13,12 @@ const workList = [
 		description: 'Acted as the Resource Management Supervisor for the division.'
 	},
 	{
-		tags: ['Oct 2022 to May 2023', 'Administrative Assistant'],
-		description: 'Processed accounts payables such as journal vouchers and invoices with purchase orders/funds reservations. Prepared staffing paperwork for HR. Provided onboarding and training for new employees.'
+		tags: ['April 2023 to May 2023', 'A/ATIP Officer'],
+		description: 'Acted as the ATIP Liaison Officer for the Pacific Region to coordinate the retrieval, review, and submission of information to the requestor.'
 	},
 	{
-		tags: ['April 2023 to May 2023', 'a/ATIP Officer'],
-		description: 'Acted as the ATIP Liaison Officer for the Pacific Region to coordinate the retrieval, review, and submission of information to the requestor.'
+		tags: ['Oct 2022 to May 2023', 'Administrative Assistant'],
+		description: 'Processed accounts payables such as journal vouchers and invoices with purchase orders/funds reservations. Prepared staffing paperwork for HR. Provided onboarding and training for new employees.'
 	}
 ]
 
